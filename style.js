2da5dac5460e9747ddebbf6ba9dc137d0f66419b
citrinestone <68588169+citrinestone@users.noreply.github.com>
2020-07-21 17:21:36 +0800

    added new website

---
+++ style.js
@@ -0,0 +1,6 @@
+const carouselSlide = document.querySelector('.mainslide');
+const carouselImages = document.querySelectorAll('.mainslide');
+
+
+const prebtn = document.querySelector('#prebtn');
+const nxtbtn = document.querySelector('#nxtbtn');
