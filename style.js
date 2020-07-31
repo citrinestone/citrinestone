c4e92679a8ef8fab34aebc117a1c5f6867a7ec3c
citrinestone <68588169+citrinestone@users.noreply.github.com>
2020-07-31 18:03:11 +0800

    Add files via upload

--- style.js
+++ style.js
@@ -1,6 +1,52 @@
-const carouselSlide = document.querySelector('.mainslide');
-const carouselImages = document.querySelectorAll('.mainslide');
+
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+var mySidebar = document.getElementById("mySidebar");
+
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+  } else {
+    mySidebar.style.display = 'block';
+  }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+// MODAL DISPLAY
+
+var certBtn = document.querySelector('.certbtn');
+var bgModal = document.querySelector('.bgmodal');
+var closemodal =document.querySelector('.close-modal');
+
+  certBtn.addEventListener('click',function(){
+
+        bgModal.classList.add('modal-active');
+  });
+    closemodal.addEventListener('click',function(){
+      bgModal.classList.remove('modal-active');
+
+    });
 
 
-const prebtn = document.querySelector('#prebtn');
-const nxtbtn = document.querySelector('#nxtbtn');
+    
